4f9f339a5f5a3df342095572d1d4b2ced124a29f
Fed Reggiardo <bksgtr@gmail.com>
2018-03-04 22:44:13 -0700

    Clean up

--- src/euler/9.clj
+++ src/euler/9.clj
@@ -1,4 +1,6 @@
-(ns euler.9)
+(ns euler.9
+  (:require
+   [euler.funcs :as f]))
 
 "A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
@@ -8,13 +10,8 @@ For example, 3(2) + 4(2) = 9 + 16 = 25 = 5(2).
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc."
 
-(defn pythagorean-triplet? [a b c]
-  (= (* c c) 
-     (+ (* a a)
-        (* b b))))
-
 (first (for [a (range 1 1000)
              b (range 1 1000)
              :let [c (- 1000 a b)]
-             :when (pythagorean-triplet? a b c)] (* a b c)))
+             :when (f/pythagorean-triplet? a b c)] (* a b c)))
 
